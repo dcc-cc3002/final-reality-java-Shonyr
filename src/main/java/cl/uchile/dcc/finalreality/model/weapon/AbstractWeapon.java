8f7611e669e3c341b9d3c2e7a199c6e2ff0eb276
Jonathan <jonathandrp@outlook.es>
2022-09-26 23:42:02 -0300

    Entrega parcial 2 completa

--- src/main/java/cl/uchile/dcc/finalreality/model/weapon/AbstractWeapon.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/weapon/AbstractWeapon.java
@@ -8,22 +8,20 @@ import java.util.Objects;
  * @author <a href="https://www.github.com/r8vnhill">R8V</a>
  * @author Jonathan Riquelme
  */
-public class Weapon {
+public abstract class AbstractWeapon {
 
   private final String name;
   private final int damage;
   private final int weight;
-  private final WeaponType type;
+
 
   /**
    * Creates a weapon with a name, a base damage, speed, and it's type.
    */
-  public Weapon(final String name, final int damage, final int weight,
-      final WeaponType type) {
+  public Weapon(final String name, final int damage, final int weight) {
     this.name = name;
     this.damage = damage;
     this.weight = weight;
-    this.type = type;
   }
 
   private String getName() {
@@ -40,10 +38,7 @@ public class Weapon {
   public int getWeight() {
     return weight;
   }
-
-  private WeaponType getType() {
-    return type;
-  }
+  
 
   @Override
   public boolean equals(final Object o) {
@@ -70,4 +65,5 @@ public class Weapon {
     return "Weapon{name='%s', damage=%d, weight=%d, type=%s}"
         .formatted(name, damage, weight, type);
   }
+
 }
